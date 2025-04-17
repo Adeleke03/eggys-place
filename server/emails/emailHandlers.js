6e688b4ddd9a392afbfa9048ff83df8fe9b4197c
Adeyemi <maysbritt30@gmail.com>
2025-04-17 15:59:24 +0100

    first commit

--- server/emails/emailHandlers.js
+++ server/emails/emailHandlers.js
@@ -1,34 +1,62 @@
-import {createTransport} from "nodemailer";
+// import {createTransport} from "nodemailer";
+// import { resetPasswordEmailTemplate } from "./emailTemplate.js";
+
+// export const sendForgotPasswordMail = (options)=>{
+
+//     const transporter = createTransport({
+//         host:process.env.EMAIL_SERVICE,
+//         port:process.env.EMAIL_PORT,
+//         secure:false,
+//         auth:{
+//             user:process.env.EMAIL_USERNAME,
+//             pass:process.env.EMAIL_PASSWORD,
+//         },
+//     });
+//     const mailOptions = {
+//         from:process.env.EMAIL_FROM,
+//         to:options.to,
+//         subject:"Reset Password",
+//         html: resetPasswordEmailTemplate(options.firstName, options.resetUrl),
+//         category: "Reset Password",
+
+//     };
+
+//     transporter.sendMail(mailOptions,function(error,info){
+//         if(error){
+//             console.log(error);
+//         }else{
+//             console.log("Email sent: " + info.response);
+//         }
+//     })
+
+// }
+
+
+
+import { createTransport } from "nodemailer";
 import { resetPasswordEmailTemplate } from "./emailTemplate.js";
 
-export const sendForgotPasswordMail = (options)=>{
-
+export const sendForgotPasswordMail = (options) => {
     const transporter = createTransport({
-        host:process.env.EMAIL_SERVICE,
-        port:process.env.EMAIL_PORT,
-        secure:false,
-        auth:{
-            user:process.env.EMAIL_USERNAME,
-            pass:process.env.EMAIL_PASSWORD,
-        },
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
     });
     const mailOptions = {
-        from:process.env.EMAIL_FROM,
-        to:options.to,
-        subject:"Reset Password",
-        html: resetPasswordEmailTemplate(options.firstName, options.resetUrl),
-        category: "Reset Password",
-
+      from: process.env.EMAIL_USERNAME,
+      to: options.to,
+      subject: "Reset Password",
+      html:  resetPasswordEmailTemplate(options.firstName, options.resetUrl),
+      category: "Reset Password",
     };
-
-    transporter.sendMail(mailOptions,function(error,info){
-        if(error){
-            console.log(error);
-        }else{
-            console.log("Email sent: " + info.response);
-        }
-    })
-
-}
-
-
+  
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+  };
